ec12decc0ca413ec68a36574d529f23aae1a72a2
Nick McCarthy <mccarthy.nick.m@gmail.com>
2023-02-12 01:30:33 -0600

    figured out that the route had a typo

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -17,36 +17,28 @@ api.get('/notes', (req, res) => {
 
 api.post('/notes', (req, res) => {
     console.log(`${req.method} note request recieved`);
-
     const { title, text } = req.body;
-
     if (title && text) {
-
         const newNote = {
             title,
             text,
             id: uuidv4()
         };
-
         const response = {
             status: 'Success',
             body: newNote
         };
-
         fs.readFile('./db/db.json', 'utf8', (err, data) => {
             if (err) {
             console.error(err);
             } else {
             const notesData = JSON.parse(data);
-    
             notesData.push(newNote);
-    
             fs.writeFile('./db/db.json', JSON.stringify(notesData, null, 4), (err) =>
             err ? console.log(err) : console.log('New note added.')
             );
             }
         });
-
         res.status(201).json(response);
     } else {
         res.status(500).json('Error adding note.')
@@ -55,23 +47,18 @@ api.post('/notes', (req, res) => {
 
 api.delete('/notes/:id', (req, res) => {
     console.log(`${req.method} note request recieved for ${req.params.id}`);
-
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
         const notesData = JSON.parse(data);
-
         for(let note of notesData){
             if (req.params.id === note.id) {
-
                 let index = notesData.indexOf(note);
                 notesData.splice(index, 1);
-
                 fs.writeFile('./db/db.json', JSON.stringify(notesData, null, 4), 
                 (err) => err ? console.log(err) : console.log('New note added.')
             );
             }
         };
     });
-
     res.json(notes);
 });
 
